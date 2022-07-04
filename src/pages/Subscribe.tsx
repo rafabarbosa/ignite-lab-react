8b33254e31e3d0476b7c82b8fc41a36062066610
Rafael Barbosa <rafaelbcon@gmail.com>
2022-07-04 16:19:37 -0300

    update image

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom"
 import { Logo } from "../components/Logo"
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import CodeMockup from '../assets/code-mockup.png';
 
 export function Subscribe () {
   const navigate = useNavigate();
@@ -63,7 +64,7 @@ export function Subscribe () {
           </form>
         </div>
       </div>
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="Code Mockup Image" />
+      <img src={CodeMockup} className="mt-10" alt="Code Mockup Image" />
     </div>
   )
 }
